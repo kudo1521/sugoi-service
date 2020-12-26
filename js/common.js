f6c3a8040a9f7e1b8f8563993ad9e18a2450320c
kudo1521 <agongteng73@gmail.com>
2020-12-26 16:57:31 +0900

    aboutとmessageを作成（下からふわっと出てくるアニメーション実装）

--- js/common.js
+++ js/common.js
@@ -1,2 +1,16 @@
 $('.slick01').slick();
 $('.catch').fadeIn(3000);
+$(function(){
+    $(".ef").css({opacity:"0"});
+    $(window).scroll(function (){
+        $('.ef').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 200){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
+    });
+});
